a3488a7ae38b4fd5db7cc004ea2bf06e972c54ee
kensvin <kensvin28@gmail.com>
2023-02-27 15:39:22 +0800

    Altimeter save

--- src/main/java/Producer/Altimeter.java
+++ src/main/java/Producer/Altimeter.java
@@ -1,54 +1,68 @@
 package Producer;
+
+import Controller.Exchange;
+import Controller.Key;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class Altimeter {
-    public static void main(String[] args) {
-        ScheduledExecutorService altimeter = Executors.newScheduledThreadPool(1);
-        altimeter.scheduleAtFixedRate(new AltimeterLogic(), 0, 3, TimeUnit.SECONDS);
-    }
-}
-
-class AltimeterLogic implements Runnable {
+public class Altimeter implements Runnable {
     Random rand = new Random();
-    String myExchange = "sensorControllerExchange";
     ConnectionFactory cf = new ConnectionFactory();
+    volatile int altitude = Integer.MIN_VALUE;
 
-    public AltimeterLogic() {
+    public Altimeter() {
     }
 
     @Override
     public void run() {
-        String currentAltitude = getData();
-        transmit(currentAltitude);
+        transmit(getAltitude());
+        setAltitude();
     }
 
-    public String getData() {
-        String altitude = "";
-        int currentAltitude = rand.nextInt(30_000, 50_000);
-        System.out.println("Altitude: " + currentAltitude);
-        if (currentAltitude > 49_000) altitude = "tooHigh";
-        else if (currentAltitude > 45_000) altitude = "slightlyHigh";
-        else if (currentAltitude > 40_000) altitude = "ok";
-        else if (currentAltitude > 35_000) altitude = "slightlyLow";
-        else if (currentAltitude >= 30_000) altitude = "tooLow";
+    public int getAltitude() {
+        if (altitude == Integer.MIN_VALUE) {
+            altitude = rand.nextInt(30_000, 50_000);
+        }
         return altitude;
     }
 
-    public void transmit(String altitude) {
+    public void setAltitude() {
+        try {
+            Connection con = cf.newConnection();
+            Channel chan = con.createChannel();
+            chan.exchangeDeclare(Exchange.ACTUATOR_SENSOR_EXCHANGE.name, "direct");
+            String qName = chan.queueDeclare().getQueue();
+            chan.basicQos(1);
+            chan.queueBind(qName, Exchange.ACTUATOR_SENSOR_EXCHANGE.name, Key.ALTITUDE.name);
+            String ctag = chan.basicConsume(qName, true, (x, msg) -> {
+                String message = new String(msg.getBody(), StandardCharsets.UTF_8);
+                int altitudeChange = Integer.parseInt(message);
+                if(altitudeChange != 0) {
+                    // TODO: Still not updating the altitude
+                    altitude = altitude + altitudeChange;
+                    System.out.println("[ALTIMETER] New Altitude: " + altitude);
+                }
+            }, x -> {
+
+            });
+            chan.basicCancel(ctag);
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void transmit(int altitude) {
         try (Connection connection = cf.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare(myExchange, "fanout");
-            channel.basicPublish(myExchange, "", false, null, altitude.getBytes());
-            System.out.println("Altitude: " + altitude);
+            channel.exchangeDeclare(Exchange.SENSOR_CONTROLLER_EXCHANGE.name, "topic");
+            channel.basicPublish(Exchange.SENSOR_CONTROLLER_EXCHANGE.name, Key.ALTITUDE.name, false, null, String.valueOf(altitude).getBytes());
+            System.out.println("[ALTIMETER] Current Altitude: " + altitude);
             Thread.sleep(100);
         } catch (IOException | TimeoutException | InterruptedException e) {
             throw new RuntimeException(e);
